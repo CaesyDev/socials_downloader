06af4bd0b1d3d06da70fadefe125c26875c64d79
CaesyDev <kenechukwuokafor7@gmail.com>
2024-02-29 17:22:36 +0100

    api updated

--- index.js
+++ index.js
@@ -64,17 +64,15 @@ app.get('/youtube', async (req, res) => {
 
 
 
-app.post('/post-make-post', async (req, res) => {
+app.post('/vidsight', async (req, res) => {
+
   try {
-   
     const remoteUrl = 'https://www.y2mate.com/mates/en912/analyzeV2/ajax';
     const requestUrl = req.body.url;
 
     const formData = {
       k_query: requestUrl
     };
-
-
     // Make the HTTP POST request using axios
     const response = await axios.post(remoteUrl, formData, {
       headers: {
@@ -84,53 +82,73 @@ app.post('/post-make-post', async (req, res) => {
       },
     });
 
-    // Handle the response as needed
-    res.status(response.status).send(response.data);
+
+    if(response.data.mess.trim().length > 0) {
+      return res.status(404).send({
+        code: 404,
+        error: "Oops! Something went wrong with that link"
+      })
+    }
+
+
+    let dataResponse = {};
+
+    if(response.data.extractor === "youtube"){
+
+      dataResponse = {
+        code:200,
+        status : response.data.status,
+        error: response.data.mess,
+        vidsource : response.data.extractor,
+        reqUrl : response.data.keyword,
+        vidtitle : response.data.title,
+        vidID: response.data.vid,
+        coverpic : `https://i.ytimg.com/vi/${response.data.vid}/0.jpg`,
+        audio : response.data.links.mp3 ?? [],
+        video : response.data.links.mp4 ?? [],
+        other : response.data.links.other ?? [],
+        author : response.data.a ?? "null",
+        similar : response.data?.related[0].contents ?? []
+      }
+
+    }else{
+
+      dataResponse = {
+        code:200,
+        status : response.data.status,
+        error: response.data.mess,
+        vidsource : response.data.extractor,
+        reqUrl : response.data.keyword,
+        vidtitle : response.data.title,
+        coverpic : response.data.thumbnail,
+        audio : response.data.links.audio ?? [],
+        video : response.data.links.video ?? [],
+        other : response.data.links.other ?? [],
+      }
+    }
+
+
+    // build new response parameters
+    res.status(200).send(dataResponse);
+
   } catch (error) {
-    console.error('Error:', error.message);
-    res.status(500).send('Internal Server Error');
+    let errorResponse = {
+      code: 500,
+      message : "An internal error occurred. Try again later"
+    }
+    res.status(500).send(errorResponse);
   }
 });
 
 
 
 
-app.get('/get-make-post', async (req, res) => {
+
+
+
+app.post('/ytsight', async (req, res) => {
+
   try {
-   
-    const remoteUrl = 'https://www.y2mate.com/mates/en912/analyzeV2/ajax';
-    const requestUrl = req.query.url;
-
-    const formData = {
-      k_query: requestUrl
-    };
-
-
-    // Make the HTTP POST request using axios
-    const response = await axios.post(remoteUrl, formData, {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'Referrer-Policy': 'strict-origin-when-cross-origin',
-        'User-Agent': 'PostmanRuntime/7.26.8',
-      },
-    });
-
-    // Handle the response as needed
-    res.status(response.status).send(response.data);
-  } catch (error) {
-    console.error('Error:', error.message);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-
-
-
-
-
-app.get('/youtube-conv', async (req, res) => {
-  try {
-   
     const remoteUrl = 'https://www.y2mate.com/mates/convertV2/index';
     const vid_ID = req.query.vid;
     const k_ID = req.query.k
@@ -157,6 +175,35 @@ app.get('/youtube-conv', async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 });
+
+
+
+
+
+
+app.get('/mine', async (req, res) => {
+
+  const options = {
+    method: 'POST',
+    url: 'https://all-media-downloader-v2.p.rapidapi.com/dl',
+    headers: {
+      'content-type': 'application/json',
+      'X-RapidAPI-Key': '660473502amsha3b31bbf2077b15p10a6b8jsn7626d7cff8e7',
+      'X-RapidAPI-Host': 'all-media-downloader-v2.p.rapidapi.com'
+    },
+    data: {
+      url: 'https://www.youtube.com/watch?v=oxFr7we3LC8'
+    }
+  };
+
+  try {
+    const response = await axios.request(options);
+    res.send(response.data);  //.requested_formats
+
+  } catch (error) {
+    res.send(error);
+  }
+})
 
 
 
